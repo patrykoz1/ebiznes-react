b08f5e3d6f3529c22c676adfdad4b5420d463044
patrykoz1 <49210844+patrykoz1@users.noreply.github.com>
2021-06-27 20:03:00 +0200

    Update fetch.js

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -2,7 +2,7 @@
 export function getProducts() {
     const requestOptions = {
         method: 'GET',
-        headers: { 'Content-Type': 'application/json','Access-Control-Allow-Origin':'https://uj-ebiznes-back.azurewebsites.net' },
+        headers: { 'Content-Type': 'application/json','Access-Control-Allow-Origin':'*' },
         mode: 'no-cors',
     };
     return fetch("https://uj-ebiznes-back.azurewebsites.net/products",requestOptions).then((response) => response.json())
