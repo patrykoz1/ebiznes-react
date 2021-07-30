0ba383e5eda9ca2bda0891108d3116a5fc1225b3
patrykoz1 <49210844+patrykoz1@users.noreply.github.com>
2021-07-31 01:17:30 +0200

    Update GoogleLogin.js

--- src/components/GoogleLogin.js
+++ src/components/GoogleLogin.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function GoogleSignIn() {
 
     const responseGoogle = () => {
-        window.location.href = "https://uj-ebiznes-front.azurewebsites.net";
+        window.location.href = "https://uj-ebiznes-back.azurewebsites.net/provider/google";
     }
 
     return (
